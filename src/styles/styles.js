6bb721fe6687bb42c44eba84caa65d6453b48647
ShubhamFistbumpDigital <shubham.fistbump@gmail.com>
2020-09-24 09:20:39 +0530

    assets added, footer without icons

---
+++ src/styles/styles.js
@@ -0,0 +1,29 @@
+export const blackText = "#000000";
+export const greyText = "#6c7174";
+export const greenText = "#3e593c";
+export const redText = "";
+export const whiteText = "#FFFFFF"
+
+
+
+// footer
+export const footerSectionOne = {
+        padding: 20,
+        backgroundColor: '#201E19',
+        color: '#A1A2A2',
+        fontSize: 16,
+        fontWeight: 500,
+        lineHeight: 1.1
+}
+export const footerSectionTwo = {
+        padding: 20,
+        backgroundColor: '#6C7174'
+}
+
+export const footerHr = {
+        borderTop: `1px solid ${whiteText}`
+}
+
+export const footerbase = {
+        color: whiteText
+}
