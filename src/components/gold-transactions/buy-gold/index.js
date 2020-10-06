4c32e3cce302feb933b70b42cd82f4d6ad146a9f
Mukesh Maurya <mukesh@insider.in>
2020-10-07 01:18:07 +0530

    create buy-gold module mobile version

--- src/components/gold-transactions/buy-gold/index.js
+++ src/components/gold-transactions/buy-gold/index.js
@@ -1,49 +1,91 @@
-import React, {useState} from 'react';
-import {Container,Row, Col, Tabs, Tab} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import {box, transactionContainer, buttonGroup, clearBtn, btn} from '../../../styles';
-import TransactionBox from '../TransactionBox';
-import CreditCard from './CreditCard';
-import Neft from './Neft';
-import NetBanking from './NetBanking';
-import Upi from './Upi';
+import React, { useState } from "react";
+import { Container, Row, Col, Tabs, Tab } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import {
+  box,
+  transactionContainer,
+  buttonGroup,
+  clearBtn,
+  btn,
+} from "../../../styles";
+import TransactionBox from "../TransactionBox";
+import CreditCard from "./CreditCard";
+import Neft from "./Neft";
+import NetBanking from "./NetBanking";
+import Upi from "./Upi";
 
 function BuyGold() {
-        const [key, setKey] = useState('credit-card');
+  const [key, setKey] = useState("credit-card");
 
-        return (
-                <Container style={transactionContainer}>
-                        <div style={{...box, padding: '18px 30px 30px'}}>
-                                <Row>
-                                        <Col md={6}>
-                                                <Tabs id="buy-gold-tabs" activeKey={key} onSelect={(k) => setKey(k)}>
-                                                        <Tab eventKey = 'credit-card' title = {<span>Credit Card</span>} style={{padding: '44px 30px 0px'}} >
-                                                                <CreditCard />
-                                                        </Tab>
-                                                        <Tab eventKey = 'neft' title = {<span>NEFT/RTGS</span>} style={{padding: '44px 30px 0px'}}>
-                                                                <Neft />
-                                                        </Tab>
-                                                        <Tab eventKey = 'net-banking' title = {<span>NetBanking</span>} style={{padding: '44px 30px 0px'}}>
-                                                                <NetBanking />
-                                                        </Tab>
-                                                        <Tab eventKey = 'upi' title = {<span>UPI</span>} style={{padding: '44px 30px 0px'}}>
-                                                                <Upi />
-                                                        </Tab>
-                                                </Tabs>
-                                        </Col>
-                                        <Col md={6}>
-                                                <div className="noInputTransactionBoxDiv">
-                                                        <TransactionBox noInput = {true} />
-                                                </div>
-                                        </Col>
-                                </Row>
-                        </div>
-                        <div className="button-group" style={buttonGroup}>
-                                <Link style={clearBtn} to="/gold-transactions">Back</Link>
-                                <Link style={{...btn, display: 'flex', justifyContent: 'center', alignItems: 'center'}} to={`/`} >Proceed</Link>
-                        </div>
-                </Container>
-        )
+  return (
+    <Container style={transactionContainer}>
+      <div style={{ ...box, padding: "18px 30px 30px" }}>
+        <Row>
+          <Col lg={6}>
+            <Tabs
+              id="buy-gold-tabs"
+              activeKey={key}
+              onSelect={(k) => setKey(k)}
+            >
+              <Tab
+                eventKey="credit-card"
+                title={<span>Credit Card</span>}
+                style={{ padding: "44px 30px 0px" }}
+                className="payment-method"
+              >
+                <CreditCard />
+              </Tab>
+              <Tab
+                eventKey="neft"
+                title={<span>NEFT/RTGS</span>}
+                style={{ padding: "44px 30px 0px" }}
+                className="payment-method"
+              >
+                <Neft />
+              </Tab>
+              <Tab
+                eventKey="net-banking"
+                title={<span>NetBanking</span>}
+                style={{ padding: "44px 30px 0px" }}
+                className="payment-method"
+              >
+                <NetBanking />
+              </Tab>
+              <Tab
+                eventKey="upi"
+                title={<span>UPI</span>}
+                style={{ padding: "44px 30px 0px" }}
+                className="payment-method"
+              >
+                <Upi />
+              </Tab>
+            </Tabs>
+          </Col>
+          <Col lg={6}>
+            <div className="noInputTransactionBoxDiv">
+              <TransactionBox noInput={true} />
+            </div>
+          </Col>
+        </Row>
+      </div>
+      <div className="button-group" style={buttonGroup}>
+        <Link style={clearBtn} to="/gold-transactions">
+          Back
+        </Link>
+        <Link
+          style={{
+            ...btn,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          to={`/`}
+        >
+          Proceed
+        </Link>
+      </div>
+    </Container>
+  );
 }
 
-export default BuyGold
+export default BuyGold;
